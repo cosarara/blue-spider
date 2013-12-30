ecb0b74a2a63819afdb5f2bdc8729b2304089e20
Jaume Delclòs Coll <cosa.rara97@gmail.com>
2013-12-30 11:10:31 +0100

    Added shebang and cProfile to speed test

--- speedtest.py
+++ speedtest.py
@@ -1,10 +1,13 @@
-
+#!/usr/bin/env python3
 # python speedtest.py ~/RH/tmp/FR.gba 0x82d4bb4 0x82d4c44
 
 from PyQt4 import QtGui
 from bluespider import mapped, mapped_gui
 import sys
 import time
+
+PROFILE = False
+
 print("imported things")
 
 app = QtGui.QApplication(sys.argv)
@@ -28,7 +31,12 @@ print("parsed stupid headers")
 print("running real test now")
 x = time.time()
 #window.load_tilesets(t1_header, t1_header, t1_imgs=None)
-window.get_tilesets(t1_header, t2_header, t1_imgs=None)
+if PROFILE:
+   import cProfile
+   import re
+   cProfile.run('window.get_tilesets(t1_header, t2_header, t1_imgs=None)')
+else:
+    window.get_tilesets(t1_header, t2_header, t1_imgs=None)
 print(time.time() - x)
 print("done")
 
